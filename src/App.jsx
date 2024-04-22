480939ac0e1696ec3f046be5d2619f62b14c265a
Vinícius Amaral <91218024+AmaralVinicius@users.noreply.github.com>
2024-04-22 19:06:17 -0300

    main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
